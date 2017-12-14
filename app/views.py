21739cacd060db41d0290c106fd19c3c2e469f39
paul rimiru <paulrimiru@gmail.com>
2017-12-14 09:41:51 +0300

    [Feature] Add support for get

--- app/views.py
+++ app/views.py
@@ -4,5 +4,8 @@ from flask import jsonify, request
 
 @APP.route('/', methods=['POST'])
 def getNearBy():
+    if request.method == 'POST':
+        resp = requests.post('http://198.199.70.165/ShareCab/index.php/api', data=request.args)
+        return resp.text
     resp = requests.post('http://198.199.70.165/ShareCab/index.php/api', data=request.args)
     return resp.text
