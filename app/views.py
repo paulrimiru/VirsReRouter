df274b409f58dc11e1c18010b8032c525ab50c97
paul rimiru <paulrimiru@gmail.com>
2017-12-14 09:59:08 +0300

    [Feature] add feature to change request args to dict)

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ from flask import jsonify, request
 @APP.route('/', methods=['POST'])
 def getNearBy():
     if request.method == 'POST':
-        resp = requests.post('http://198.199.70.165/ShareCab/index.php/api', data=request.args)
+        resp = requests.post('http://198.199.70.165/ShareCab/index.php/api', data=request.args.to_dict())
         return resp.text
-    resp = requests.post('http://198.199.70.165/ShareCab/index.php/api', data=request.args)
+    resp = requests.post('http://198.199.70.165/ShareCab/index.php/api', data=request.args.to_dict())
     return resp.text
