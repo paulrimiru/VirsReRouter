bd398512521d200a7dd85ed64b6d8bf42b73b0b6
paul rimiru <paulrimiru@gmail.com>
2017-12-14 16:05:34 +0300

    Add spring compatibility

--- app/views.py
+++ app/views.py
@@ -9,3 +9,10 @@ def getNearBy():
         return resp.text
     resp = requests.post('http://198.199.70.165/ShareCab/index.php/api', data=request.args.to_dict())
     return resp.text
+@APP.route('/wahindi/<endpoint>', methods=['POST'])
+def wahindiUrl(endpoint):
+    if request.method == 'POST':
+        resp = requests.post('http://198.199.70.165:8080/ShareCab/'+endpoint, data=request.args.to_dict())
+        return resp.text
+    resp = requests.post('http://198.199.70.165:8080/ShareCab/'+endpoint, data=request.args.to_dict())
+    return resp.text
